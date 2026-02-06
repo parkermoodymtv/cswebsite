5fe6538ac62867fdde7f0e8392b50b57833aa178
Parker Moody <parker.moody.mtv@scienceandtech.org>
2026-02-06 15:01:14 -0700

    changed homepage

--- clicker.js
+++ clicker.js
@@ -5,16 +5,13 @@ const button = document.getElementById("cakeclicker");
 function clickCake() {
     clicks += 1;
     counter.innerHTML = "Clicks: " + (clicks).toLocaleString();
-    // alert("You clicked the cake! Total clicks: " + (clicks).toLocaleString());
 }
 
-// document.getElementById("clickcount").onclick = clickCake;
-
-document.addEventListener("DOMContentLoaded", function () {
-    button.addEventListener("click", clickCake); // Attach the function to the 'click' event
+document.addEventListener("DOMContentLoaded", function () { //Connect the click to the image
+    button.addEventListener("click", clickCake);
 });
 
-document.body.onkeyup = function (e) {
+document.body.onkeyup = function (e) { //Connect the click to the space bar
     if (e.key == " " || e.code == "Space" || e.keyCode == 32) {
         clickCake();
     }
