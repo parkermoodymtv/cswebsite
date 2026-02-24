1217f1f47ca7ed926e0e0588f4eb391a51cc7378
Parker Moody <parker.moody.mtv@scienceandtech.org>
2026-02-24 16:26:11 -0700

    sound affects for cake clickers

--- clicker.js
+++ clicker.js
@@ -1,10 +1,19 @@
 let clicks = 0;
 const counter = document.getElementById("clickcount");
 const button = document.getElementById("cakeclicker");
+const audio1 = document.getElementById("boing64");
+const audio2 = document.getElementById("boingsound");
 
 function clickCake() {
     clicks += 1;
     counter.innerHTML = "Clicks: " + (clicks).toLocaleString();
+    if (Math.random() >= 0.9) {
+        audio1.currentTime = 0;
+        audio1.play();
+    } else {
+        audio2.currentTime = 0;
+        audio2.play();
+    }
 }
 
 document.addEventListener("DOMContentLoaded", function () { //Connect the click to the image
--- clickerV2.js
+++ clickerV2.js
@@ -6,6 +6,8 @@ https://stackoverflow.com/a/55668864
 let clicks = 0;
 const counter = document.getElementById("clickcount");
 const button = document.getElementById("cakeclicker");
+const audio1 = document.getElementById("boing64");
+const audio2 = document.getElementById("boingsound");
 
 function clickCake() {
     clicks += 1;
@@ -14,6 +16,14 @@ function clickCake() {
     button.style.position = 'absolute';
     button.style.top = document.body.clientHeight * Math.random() * 0.9 + 'px';
     button.style.left = document.body.clientWidth * Math.random() * 0.9 + 'px';
+
+    if (clicks % 4 == 0) {
+        audio1.currentTime = 0;
+        audio1.play();
+    } else {
+        audio2.currentTime = 0;
+        audio2.play();
+    }
 }
 
 document.addEventListener("DOMContentLoaded", function () { //Connect the click to the image
